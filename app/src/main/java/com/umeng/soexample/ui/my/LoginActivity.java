5ca943eee821d3b5cfaaee94c8c91f9740f1273b
yuanwenb <2118415487@qq.com>
2020-12-22 10:41:17 +0800

    分类页面

---
+++ app/src/main/java/com/umeng/soexample/ui/my/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.umeng.soexample.ui.my;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.umeng.soexample.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
